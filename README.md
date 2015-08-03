283c85d52979e2eaff1fbf92532340378e3fcf71
ryan-mcbride <mcbrideryan89@gmail.com>
2015-08-03 16:54:42 -0700

    reinitialized because mistakes were made

---
+++ README.md
@@ -0,0 +1,25 @@
+# PropsBot #
+
+
+## A Slackbot for props ##
+
+
+PropsBot is an awesome robot that monitors the public channels of any team Slack server. When you want to give Albrey some props, you just have to type `albrey++` and PropsBot will incriment his current points.
+
+## Problem ##
+There is currently no easy way to give props to someone. High fives are cool, but it's hard to keep track of the number you've racked up over the past few days, and without that number, how will you know how cool you are?
+
+## Solution ##
+PropsBot takes all the guess work out of who did the awesomest stuff. People can give points to people who made coffee, brought donuts, helped them with a problem, or made a cool slack bot.
+
+## Quote from the CEO ##
+  > I figuratively cannot breathe without PropsBot. Literally.
+
+## How to Get Started ##
+Just plug PropsBot into your slack channel and props away!
+
+## Customer Quote ##
+  > I never knew what love was until the day I got PropsBot.
+
+
+What are you waiting for? Hop in slack and start giving people points!
---
+++ propsbot.py
@@ -0,0 +1,29 @@
+import time
+from slackclient import SlackClient
+
+token = "token"
+sc = SlackClient(token)
+chan = "channel"
+
+print sc.api_call("channels.info", channel=chan)
+# sc.api_call("chat.postMessage", channel="C08H7Q3T6", text="HELLO")
+
+def sendMess(mess):
+  sc.api_call("chat.postMessage", channel=chan, text=mess.replace("+", ""), username="PropsBot")
+# function to send message to chat
+
+if sc.rtm_connect():
+  while True:
+    events = sc.rtm_read()
+    for event in events:
+      if event["type"] == "message" and "text" in event: # check for message events
+        message = event["text"].split(' ', 1) # split first word out of message
+        first = message[0] # get first word
+        if(first[-2:] == "++"):
+          if(len(message) == 1):
+            sendMess("{thing} just got props!".format(thing=first.encode("utf-8"))) 
+          else:
+            sendMess("{thing} just got props {reason}!".format(thing=first.encode("utf-8"), reason=message[1].encode("utf-8")))
+    time.sleep(1) # timeout for checking new messages
+else:
+  print "Connection Failed, invalid token?"
