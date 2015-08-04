b986f0212e8a764071d00b983ad2db199bad00cc
ryan-mcbride <mcbrideryan89@gmail.com>
2015-08-04 10:00:14 -0700

    implemented -- functionality

--- propsbot.py
+++ propsbot.py
@@ -3,23 +3,29 @@ import sqlite3 as lite
 import sys
 from slackclient import SlackClient
 
-token = "token"
+
 sc = SlackClient(token)
-chan = "chan"
+
 
 # db initialization
-def propsdb(thing):
+def propsdb(thing, posneg):
   con = lite.connect('props.db')
  
   cursor = con.execute("SELECT * FROM Props WHERE Thing = '{storething}';".format(storething=thing))
   row = cursor.fetchone()
 
   if row == None:
-    val = 1
+    if(posneg == "neg"):
+      val = -1
+    else:
+      val = 1
     con.execute("INSERT INTO Props (Thing, Points) VALUES ('{newthing}', 1);".format(newthing=thing))
     con.commit()
   else:
-    val = row[1] + 1
+    if(posneg == "neg"):
+      val = row[1] - 1
+    else:
+      val = row[1] + 1
     con.execute("UPDATE Props SET Points = {newval} WHERE Thing = '{storething}';".format(newval=val, storething=thing))
     con.commit()
     
@@ -27,9 +33,10 @@ def propsdb(thing):
   return val
 
 def sendMess(mess):
-  cleanName = mess.replace("+", "")
+  cleanName = mess.replace("+", "").replace("--", "")
   sc.api_call("chat.postMessage", channel=chan, text=cleanName, username="PropsBot", icon_url="https://salesforceyoda.files.wordpress.com/2014/10/fist-slide.jpg")
 # function to send message to chat
+
 
 if sc.rtm_connect():
   print "PropsBot is ready"
@@ -41,9 +48,14 @@ if sc.rtm_connect():
         first = message[0] # get first word
         if(first[-2:] == "++"):
           if(len(message) == 1):
-            sendMess("{thing} now has {num} points! :thumbsup:".format(thing=first.encode("utf-8"), num=propsdb(first.replace("+", "").encode("utf-8")))) 
+            sendMess("{thing} now has {num} points! :thumbsup:".format(thing=first.encode("utf-8"), num=propsdb(first.replace("+", "").encode("utf-8"), "pos"))) 
           else:
-            sendMess("{thing} just got props {reason} for a total of {num} points! :thumbsup:".format(thing=first.encode("utf-8"), reason=message[1].encode("utf-8"), num=propsdb(first.replace("+", "").encode("utf-8"))))
+            sendMess("{thing} just got props {reason} for a total of {num} points! :thumbsup:".format(thing=first.encode("utf-8"), reason=message[1].encode("utf-8"), num=propsdb(first.replace("+", "").encode("utf-8"), "pos")))
+        elif(first[-2:] == "--"):
+          if(len(message) == 1):
+            sendMess("{thing} now has {num} points! :thumbsdown:".format(thing=first.encode("utf-8"), num=propsdb(first.replace("-", "").encode("utf-8"), "neg")))
+          else:
+            sendMess("{thing} just got !props {reason} for a total of {num} points! :thumbsdown:".format(thing=first.encode("utf-8"), reason=message[1].encode("utf-8"), num=propsdb(first.replace("-", "").encode("utf-8"), "neg")))
     time.sleep(1) # timeout for checking new messages
 else:
   print "Connection Failed, invalid token?"
