af57c3fb92f42594b3b9e77a178e473945f1489c
ryan-mcbride <mcbrideryan89@gmail.com>
2015-08-03 17:25:11 -0700

    prettified a bit, next step is db

--- propsbot.py
+++ propsbot.py
@@ -1,9 +1,9 @@
 import time
 from slackclient import SlackClient
 
-token = "token"
+token = ""
 sc = SlackClient(token)
-chan = "channel"
+chan = ""
 
 print sc.api_call("channels.info", channel=chan)
 # sc.api_call("chat.postMessage", channel="C08H7Q3T6", text="HELLO")
@@ -21,9 +21,10 @@ if sc.rtm_connect():
         first = message[0] # get first word
         if(first[-2:] == "++"):
           if(len(message) == 1):
-            sendMess("{thing} just got props!".format(thing=first.encode("utf-8"))) 
+            sendMess("{thing} just got props! :thumbsup:".format(thing=first.encode("utf-8"))) 
           else:
-            sendMess("{thing} just got props {reason}!".format(thing=first.encode("utf-8"), reason=message[1].encode("utf-8")))
+            sendMess("{thing} just got props {reason}! :thumbsup:".format(thing=first.encode("utf-8"), reason=message[1].encode("utf-8")))
     time.sleep(1) # timeout for checking new messages
+    # TODO: implement a database to keep track of props
 else:
   print "Connection Failed, invalid token?"
