f1b183614ed3439d19cabb6ec82f1b77ffa027c3
ryan-mcbride <mcbrideryan89@gmail.com>
2015-08-03 19:40:52 -0700

    insertion db query is working

--- propsbot.py
+++ propsbot.py
@@ -1,15 +1,39 @@
 import time
+import sqlite3 as lite
+import sys
 from slackclient import SlackClient
 
 token = ""
 sc = SlackClient(token)
 chan = ""
 
+# db initialization
+
+def propsdb(thing):
+    con = lite.connect('props.db')
+   
+    cursor = con.execute("SELECT * FROM Props WHERE Thing = '{storething}';".format(storething=thing))
+    
+    if cursor.fetchone() == None:
+      print "if"
+      con.execute("INSERT INTO Props (Thing, Points) VALUES ('{newthing}', 1);".format(newthing=thing))
+      con.commit()
+      #Insert New Row
+    else:
+      print "else"
+      #Increment Existing Row
+    
+    
+    #for row in cursor:
+      # print row
+    
+    con.close()
+
 print sc.api_call("channels.info", channel=chan)
-# sc.api_call("chat.postMessage", channel="C08H7Q3T6", text="HELLO")
 
 def sendMess(mess):
-  sc.api_call("chat.postMessage", channel=chan, text=mess.replace("+", ""), username="PropsBot")
+  cleanName = mess.replace("+", "")
+  sc.api_call("chat.postMessage", channel=chan, text=cleanName, username="PropsBot")
 # function to send message to chat
 
 if sc.rtm_connect():
@@ -21,6 +45,7 @@ if sc.rtm_connect():
         first = message[0] # get first word
         if(first[-2:] == "++"):
           if(len(message) == 1):
+            propsdb(first.replace("+", "").encode("utf-8"))
             sendMess("{thing} just got props! :thumbsup:".format(thing=first.encode("utf-8"))) 
           else:
             sendMess("{thing} just got props {reason}! :thumbsup:".format(thing=first.encode("utf-8"), reason=message[1].encode("utf-8")))
