afb5baeb6d8a53818c7a1b92fb8c632c1ae8ab65
ryan-mcbride <mcbrideryan89@gmail.com>
2015-08-04 12:14:44 -0700

    added universal utf-8, tweaked formatting

--- propsbot.py
+++ propsbot.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import time
 import sqlite3 as lite
 import sys
@@ -10,7 +12,7 @@ sc = SlackClient(token)
 def propsdb(thing, posneg):
   con = lite.connect('props.db')
  
-  cursor = con.execute("SELECT * FROM Props WHERE Thing = '{storething}';".format(storething=thing))
+  cursor = con.execute("SELECT * FROM Props WHERE Thing = '{storething}';".format(storething=thing.lower()))
   row = cursor.fetchone()
 
   if row == None:
@@ -18,14 +20,14 @@ def propsdb(thing, posneg):
       val = -1
     else:
       val = 1
-    con.execute("INSERT INTO Props (Thing, Points) VALUES ('{newthing}', 1);".format(newthing=thing))
+    con.execute("INSERT INTO Props (Thing, Points) VALUES ('{newthing}', 1);".format(newthing=thing.lower()))
     con.commit()
   else:
     if(posneg == "neg"):
       val = row[1] - 1
     else:
       val = row[1] + 1
-    con.execute("UPDATE Props SET Points = {newval} WHERE Thing = '{storething}';".format(newval=val, storething=thing))
+    con.execute("UPDATE Props SET Points = {newval} WHERE Thing = '{storething}';".format(newval=val, storething=thing.lower()))
     con.commit()
     
   con.close()
@@ -33,7 +35,7 @@ def propsdb(thing, posneg):
 
 # sends message to slack
 def sendMess(mess):
-  cleanName = mess.replace("+", "").replace("--", "")
+  cleanName = mess.replace("++", "").replace("--", "").replace("—", "")
   sc.api_call("chat.postMessage", channel=chan, text=cleanName, username="PropsBot", icon_url="https://salesforceyoda.files.wordpress.com/2014/10/fist-slide.jpg")
 
 # gets top props list
@@ -60,16 +62,20 @@ if sc.rtm_connect():
         if(first[-2:] == "++"):
           if(len(message) == 1):
             #TODO: Break props messages out into a separate function
-            sendMess("{thing} now has {num} points! :thumbsup:".format(thing=first.encode("utf-8"), num=propsdb(first.replace("+", "").encode("utf-8"), "pos"))) 
+            sendMess("{thing} now has {num} points! :thumbsup:".format(thing=first, num=propsdb(first.replace("+", ""), "pos"))) 
           else:
-            sendMess("{thing} just got props {reason} for a total of {num} points! :thumbsup:".format(thing=first.encode("utf-8"), reason=message[1].encode("utf-8"), num=propsdb(first.replace("+", "").encode("utf-8"), "pos")))
+            sendMess("{thing} just got props {reason} for a total of {num} points! :thumbsup:".format(thing=first, reason=message[1], num=propsdb(first.replace("+", ""), "pos")))
         elif(first[-2:] == "--"):
           if(len(message) == 1):
-            sendMess("{thing} now has {num} points! :thumbsdown:".format(thing=first.encode("utf-8"), num=propsdb(first.replace("-", "").encode("utf-8"), "neg")))
+            sendMess("{thing} now has {num} points! :thumbsdown:".format(thing=first, num=propsdb(first.replace("-", ""), "neg")))
           else:
-            sendMess("{thing} just got !props {reason} for a total of {num} points! :thumbsdown:".format(thing=first.encode("utf-8"), reason=message[1].encode("utf-8"), num=propsdb(first.replace("-", "").encode("utf-8"), "neg")))
+            sendMess("{thing} just got !props {reason} for a total of {num} points! :thumbsdown:".format(thing=first, reason=message[1], num=propsdb(first.replace("-", ""), "neg")))
         elif(first == "PropsBot.list"):
+          try:
+            val = int(message[1])
             getList(message[1])
+          except:
+            sendMess("I need an integer for that function!")
     time.sleep(1) # timeout for checking new messages
 else:
   print "Connection Failed, invalid token?"
